125baf28e2b3f7476283841009f94e619810afeb
Dwip Patel <dwipatel00@hotmail.com>
2019-12-01 18:18:26 -0500

    finished server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,22 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 4000;
 
+const mongoose = require('mongoose');
+let db_url = "mongodb+srv://Admin:Admin@cluster0-o8ivn.mongodb.net/test?retryWrites=true&w=majority";
+let mongodb = db_url;
+mongoose.connect(mongodb);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('connected', function() {
+    console.log('Connected to database');
+});
+db.on('disconnected', function() {
+    console.log('Disconnected from database');   
+});
+
+
 app.use(cors());
 app.use(bodyParser.json());
 
